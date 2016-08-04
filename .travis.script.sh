30136397bbebd7f20d4b5690f73cf0a7635e04f9
Artur Signell <artur@vaadin.com>
2016-08-04 11:22:40 +0300

    Enable Travis build for pull requests and master

---
+++ .travis.script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# TRAVIS_PULL_REQUEST == "false" for a normal branch commit, the PR number for a PR
+# TRAVIS_BRANCH == target of normal commit or target of PR
+# TRAVIS_SECURE_ENV_VARS == true if encrypted variables are available
+# TRAVIS_REPO_SLUG == the repository, e.g. vaadin/framework8-demo
+
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
+then
+	# Secure vars available, needed for TestBench tests
+	# Pull request inside repository or branch build
+	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE verify
+fi
