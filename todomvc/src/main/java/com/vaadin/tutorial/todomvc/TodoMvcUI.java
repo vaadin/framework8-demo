b421a7bfcbfdfffe15cd379fa77b6e35edf6fb04
Pekka Hyvönen <pekka@vaadin.com>
2016-08-30 12:18:30 +0300

    Initial implementation of TodoMVC demo
    Lots of missing features:
    - mark todo done in first grid column
    - remove todo from last grid column
    - inline editing of todo inside grid
    - jdbc integration ?

---
+++ todomvc/src/main/java/com/vaadin/tutorial/todomvc/TodoMvcUI.java
@@ -0,0 +1,48 @@
+package com.vaadin.tutorial.todomvc;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.themes.ValoTheme;
+
+/* User Interface written in Java.
+ *
+ * Define the user interface shown on the Vaadin generated web page by extending the UI class.
+ * By default, a new UI instance is automatically created when the page is loaded. To reuse
+ * the same instance, add @PreserveOnRefresh.
+ */
+@Title("TodoMVC")
+@Theme(ValoTheme.THEME_NAME)
+public class TodoMvcUI extends UI {
+
+    /*
+     * The "Main method".
+     *
+     * This is the entry point method executed to initialize and configure the
+     * visible user interface. Executed on every browser reload because a new
+     * instance is created for each web page loaded.
+     */
+    @Override
+    protected void init(VaadinRequest request) {
+        setSizeUndefined();
+        setContent(new TodoViewImpl());
+    }
+
+    /*
+     * Deployed as a Servlet or Portlet.
+     *
+     * You can specify additional servlet parameters like the URI and UI class
+     * name and turn on production mode when you have finished developing the
+     * application.
+     */
+    @WebServlet(urlPatterns = "/*")
+    @VaadinServletConfiguration(ui = TodoMvcUI.class, productionMode = false, widgetset = "com.vaadin.v7.Vaadin7WidgetSet")
+    public static class MyUIServlet extends VaadinServlet {
+    }
+
+}
