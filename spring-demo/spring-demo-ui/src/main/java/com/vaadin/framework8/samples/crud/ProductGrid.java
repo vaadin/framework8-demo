813d1b51afb35cb63019521d9272b39e334b935a
Teemu Suo-Anttila <tsuoanttila@users.noreply.github.com>
2016-09-14 14:42:32 +0300

    Use new Grid for Spring demo (#23)

--- spring-demo/spring-demo-ui/src/main/java/com/vaadin/framework8/samples/crud/ProductGrid.java
+++ spring-demo/spring-demo-ui/src/main/java/com/vaadin/framework8/samples/crud/ProductGrid.java
@@ -2,154 +2,79 @@ package com.vaadin.framework8.samples.crud;
 
 import java.util.Collection;
 import java.util.Locale;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.vaadin.framework8.samples.backend.data.Availability;
+import com.vaadin.framework8.samples.backend.data.Category;
 import com.vaadin.framework8.samples.backend.data.Product;
 import com.vaadin.server.FontAwesome;
-import com.vaadin.v7.data.util.BeanItem;
-import com.vaadin.v7.data.util.BeanItemContainer;
-import com.vaadin.v7.data.util.MethodProperty;
-import com.vaadin.v7.data.util.converter.StringToEnumConverter;
-import com.vaadin.v7.data.util.converter.StringToIntegerConverter;
-import com.vaadin.v7.data.util.filter.Or;
-import com.vaadin.v7.data.util.filter.SimpleStringFilter;
-import com.vaadin.v7.ui.Grid;
-import com.vaadin.v7.ui.renderers.HtmlRenderer;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.renderers.HtmlRenderer;
+import com.vaadin.ui.renderers.NumberRenderer;
 
 /**
  * Grid of products, handling the visual presentation and filtering of a set of
  * items. This version uses an in-memory data source that is suitable for small
  * data sets.
  */
-public class ProductGrid extends Grid {
-
-    private StringToEnumConverter availabilityConverter = new StringToEnumConverter() {
-        @Override
-        public String convertToPresentation(Enum availability,
-                java.lang.Class<? extends String> targetType, Locale locale)
-                throws ConversionException {
-            String text = super.convertToPresentation(availability, targetType,
-                    locale);
-
-            String color = "";
-            if (availability == Availability.AVAILABLE) {
-                color = "#2dd085";
-            } else if (availability == Availability.COMING) {
-                color = "#ffc66e";
-            } else if (availability == Availability.DISCONTINUED) {
-                color = "#f54993";
-            }
-
-            String iconCode = "<span class=\"v-icon\" style=\"font-family: "
-                    + FontAwesome.CIRCLE.getFontFamily() + ";color:" + color
-                    + "\">&#x"
-                    + Integer.toHexString(FontAwesome.CIRCLE.getCodepoint())
-                    + ";</span>";
-
-            return iconCode + " " + text;
-        };
-    };
+public class ProductGrid extends Grid<Product> {
 
     public ProductGrid() {
         setSizeFull();
 
-        setSelectionMode(SelectionMode.SINGLE);
-
-        BeanItemContainer<Product> container = new BeanItemContainer<>(
-                Product.class);
-        setContainerDataSource(container);
-        setColumnOrder("id", "productName", "price", "availability",
-                "stockCount", "category");
-
-        // Show empty stock as "-"
-        getColumn("stockCount").setConverter(new StringToIntegerConverter() {
-            @Override
-            public String convertToPresentation(Integer value,
-                    java.lang.Class<? extends String> targetType, Locale locale)
-                    throws ConversionException {
-                if (value == 0) {
-                    return "-";
-                }
-
-                return super.convertToPresentation(value, targetType, locale);
-            };
-        });
-
-        // Add an traffic light icon in front of availability
-        getColumn("availability").setConverter(availabilityConverter)
-                .setRenderer(new HtmlRenderer());
-
-        // Add " €" automatically after price
-        getColumn("price").setConverter(new EuroConverter());
-
-        // Show categories as a comma separated list
-        getColumn("category").setConverter(new CollectionToStringConverter());
-
-        // Align columns using a style generator and theme rule until #15438
-        setCellStyleGenerator(new CellStyleGenerator() {
-
-            @Override
-            public String getStyle(CellReference cellReference) {
-                if (cellReference.getPropertyId().equals("price")
-                        || cellReference.getPropertyId().equals("stockCount")) {
-                    return "align-right";
-                }
-                return null;
-            }
-        });
+        addColumn("Id", p -> String.valueOf(p.getId()));
+        addColumn("Product Name", Product::getProductName);
+        addColumn("Price", Product::getPrice, new NumberRenderer())
+                .setStyleGenerator(c -> "align-right");
+        addColumn("Availability", p -> {
+            Availability availability = p.getAvailability();
+            return getTrafficLightIconHtml(availability) + " "
+                    + availability.name();
+        }, new HtmlRenderer());
+        addColumn("Stock Count", Product::getStockCount, new NumberRenderer())
+                .setStyleGenerator(c -> "align-right");
+        addColumn("Category", p -> p.getCategory().stream()
+                .map(Category::getName).collect(Collectors.joining(", ")));
     }
 
-    /**
-     * Filter the grid based on a search string that is searched for in the
-     * product name, availability and category columns.
-     *
-     * @param filterString
-     *            string to look for
-     */
-    public void setFilter(String filterString) {
-        getContainer().removeAllContainerFilters();
-        if (filterString.length() > 0) {
-            SimpleStringFilter nameFilter = new SimpleStringFilter(
-                    "productName", filterString, true, false);
-            SimpleStringFilter availabilityFilter = new SimpleStringFilter(
-                    "availability", filterString, true, false);
-            SimpleStringFilter categoryFilter = new SimpleStringFilter(
-                    "category", filterString, true, false);
-            getContainer().addContainerFilter(
-                    new Or(nameFilter, availabilityFilter, categoryFilter));
+    private String getTrafficLightIconHtml(Availability availability) {
+        String color = "";
+        if (availability == Availability.AVAILABLE) {
+            color = "#2dd085";
+        } else if (availability == Availability.COMING) {
+            color = "#ffc66e";
+        } else if (availability == Availability.DISCONTINUED) {
+            color = "#f54993";
         }
 
+        String iconCode = "<span class=\"v-icon\" style=\"font-family: "
+                + FontAwesome.CIRCLE.getFontFamily() + ";color:" + color
+                + "\">&#x"
+                + Integer.toHexString(FontAwesome.CIRCLE.getCodepoint())
+                + ";</span>";
+        return iconCode;
     }
 
-    private com.vaadin.v7.data.util.BeanItemContainer<Product> getContainer() {
-        return (BeanItemContainer<Product>) super.getContainerDataSource();
-    }
-
-    @Override
-    public Product getSelectedRow() throws IllegalStateException {
-        return (Product) super.getSelectedRow();
-    }
-
-    public void setProducts(Collection<Product> products) {
-        getContainer().removeAllItems();
-        getContainer().addAll(products);
+    public Product getSelectedRow() {
+        return getSelectedItem().orElse(null);
     }
 
     public void refresh(Product product) {
-        // We avoid updating the whole table through the backend here so we can
-        // get a partial update for the grid
-        BeanItem<Product> item = getContainer().getItem(product);
-        if (item != null) {
-            // Updated product
-            MethodProperty p = (MethodProperty) item.getItemProperty("id");
-            p.fireValueChange();
-        } else {
-            // New product
-            getContainer().addBean(product);
-        }
+        getDataCommunicator().refresh(product);
     }
 
-    public void remove(Product product) {
-        getContainer().removeItem(product);
+    public void setFilter(String text) {
+        Predicate<Product> filter = null;
+        if (text != null && !text.isEmpty()) {
+            filter = t -> Stream
+                    .of(t.getProductName(), t.getAvailability(),
+                            t.getCategory())
+                    .map(String::valueOf)
+                    .anyMatch(s -> s.toLowerCase(getLocale())
+                            .contains(text.toLowerCase(getLocale())));
+        }
+        getDataCommunicator().setInMemoryFilter(filter);
     }
 }
