a3c082933b895657daa2e24c0572737e652b8d44
Artur <artur@vaadin.com>
2016-09-14 12:48:02 +0300

    Demo for using a REST API directly from Grid through JsonObjects (#22)

---
+++ rest-json-datasource-demo/src/main/java/com/vaadin/framework8/demo/restjson/RestDataSource.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.framework8.demo.restjson;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.vaadin.data.util.JsonUtil;
+import com.vaadin.server.data.DataSource;
+import com.vaadin.server.data.Query;
+
+import elemental.json.Json;
+import elemental.json.JsonArray;
+import elemental.json.JsonObject;
+
+public class RestDataSource implements DataSource<JsonObject> {
+
+    private String restApiUrl;
+
+    public RestDataSource(String restApiUrl) {
+        this.restApiUrl = restApiUrl;
+    }
+
+    @Override
+    public Stream<JsonObject> apply(Query t) {
+        URL url;
+        try {
+            url = new URL(restApiUrl);
+            String jsonData = IOUtils.toString(url, StandardCharsets.UTF_8);
+            JsonObject json = Json.parse(jsonData);
+
+            JsonArray results = json.getArray("results");
+            return JsonUtil.objectStream(results);
+        } catch (IOException e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,
+                    "Error fetching JSON", e);
+            // Must return something which matches size, or grid will keep
+            // asking and asking...
+            return IntStream.range(0, 200).mapToObj(i -> Json.createObject());
+        }
+    };
+
+    @Override
+    public boolean isInMemory() {
+        return false;
+    }
+
+    @Override
+    public int size(Query t) {
+        return 200;
+    }
+
+}
