04c4d10ee644c92a067fa205b389b07757f382a7
Teemu Suo-Anttila <teemusa@vaadin.com>
2016-09-27 15:05:46 +0300

    Add TODO comment about filtering

--- spring-demo/spring-demo-ui/src/main/java/com/vaadin/framework8/samples/crud/ProductDataSource.java
+++ spring-demo/spring-demo-ui/src/main/java/com/vaadin/framework8/samples/crud/ProductDataSource.java
@@ -10,8 +10,13 @@ public interface ProductDataSource extends DataSource<Product> {
 
     /**
      * Sets the filtering text for this DataSource.
+     * <p>
+     * TODO: This method should be removed once more generic filtering support
+     * is implemented. Should be replaced by a filter provided through the
+     * component itself.
      * 
      * @param filterText
+     *          the filtering text
      */
     public void setFilterText(String filterText);
 
