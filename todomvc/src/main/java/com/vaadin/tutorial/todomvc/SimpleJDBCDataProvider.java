ba29c8909e72ad95a9b85584a2755c2c161ccb66
elmot <elmot@vaadin.com>
2016-11-29 15:14:21 +0200

    Fix Query generics in TodoMVC

--- todomvc/src/main/java/com/vaadin/tutorial/todomvc/SimpleJDBCDataProvider.java
+++ todomvc/src/main/java/com/vaadin/tutorial/todomvc/SimpleJDBCDataProvider.java
@@ -52,14 +52,14 @@ public class SimpleJDBCDataProvider<T> extends AbstractJDBCDataProvider<T> {
 
     @Override
     protected ResultSet rowCountStatement(Connection connection,
-            Query<Void> query) throws SQLException {
+            Query<T,Void> query) throws SQLException {
         assert query.getSortOrders() == null || query.getSortOrders().isEmpty();
         return sizeStatement.executeQuery();
     }
 
     @Override
     protected ResultSet resultSetStatement(
-            Query<Void> query) throws SQLException {
+            Query<T,Void> query) throws SQLException {
         assert query.getSortOrders() == null || query.getSortOrders().isEmpty();
         return resultSetStatement.executeQuery();
     }
