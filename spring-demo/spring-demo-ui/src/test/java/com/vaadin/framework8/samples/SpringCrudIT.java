62462584dbd97dea81124f3460986ef9fa48af06
Henri Sara <hesara@vaadin.com>
2017-02-02 15:00:53 +0200

    Fix/simplify/update dependencies and update for latest framework

--- spring-demo/spring-demo-ui/src/test/java/com/vaadin/framework8/samples/SpringCrudIT.java
+++ spring-demo/spring-demo-ui/src/test/java/com/vaadin/framework8/samples/SpringCrudIT.java
@@ -35,7 +35,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.vaadin.demo.testutil.AbstractDemoTest;
@@ -50,7 +50,7 @@ import com.vaadin.testbench.elements.PasswordFieldElement;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TestConfig.class)
+@SpringBootTest(classes = TestConfig.class)
 public class SpringCrudIT extends AbstractDemoTest {
 
     @Autowired
