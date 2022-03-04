65185cd21f8a555b8af337c944c3164102a0781f
Manolo Carrasco <manolo@vaadin.com>
2022-03-04 14:34:21 +0200

    step 3: addressbook: port v8 UI to v23 router

--- addressbook/src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ addressbook/src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -1,14 +1,7 @@
 package com.vaadin.tutorial.addressbook;
 
-import javax.servlet.annotation.WebServlet;
-
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Title;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.router.Route;
 
 /* User Interface written in Java.
  *
@@ -16,53 +9,8 @@ import com.vaadin.ui.themes.ValoTheme;
  * By default, a new UI instance is automatically created when the page is loaded. To reuse
  * the same instance, add @PreserveOnRefresh.
  */
-@Title("Addressbook")
-@Theme(ValoTheme.THEME_NAME)
-public class AddressbookUI extends UI {
 
-    /*
-     * The "Main method".
-     *
-     * This is the entry point method executed to initialize and configure the
-     * visible user interface. Executed on every browser reload because a new
-     * instance is created for each web page loaded.
-     */
-    @Override
-    protected void init(VaadinRequest request) {
-        buildLayout();
-    }
-
-    @Override
-    public MainLayout getContent() {
-        return (MainLayout) super.getContent();
-    }
-
-    /*
-     * Robust layouts.
-     *
-     * Layouts are components that contain other components. HorizontalLayout
-     * contains TextField and Button. It is wrapped with a Grid into
-     * VerticalLayout for the left side of the screen. Allow user to resize the
-     * components with a SplitPanel.
-     *
-     * MainLayout is set up declaratively here. See its internals and
-     * declarative HTML files in resources folder.
-     */
-    private void buildLayout() {
-        // Split and allow resizing
-        setContent(new MainLayout());
-    }
-
-    /*
-     * Deployed as a Servlet or Portlet.
-     *
-     * You can specify additional servlet parameters like the URI and UI class
-     * name and turn on production mode when you have finished developing the
-     * application.
-     */
-    @WebServlet(urlPatterns = "/*")
-    @VaadinServletConfiguration(ui = AddressbookUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+@Route("")
+public class AddressbookUI extends MainLayout implements AppShellConfigurator {
 
 }
